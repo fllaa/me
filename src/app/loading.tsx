8af60f4361838468cba36c5a8aac6162744ec405
Fallah Andy Prakasa <me@flla.my.id>
2024-02-01 21:51:53 +0700

    feat: create new loader for initial page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-import ShuffleLoader from "@me/components/loader/shuffle-loader";
-
 export default function Loading() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center ">
-      <ShuffleLoader />
+    <div className="fixed inset-0 z-20 flex h-screen w-screen flex-col items-center justify-center gap-4 bg-background transition-colors duration-300 dark:bg-background-dark">
+      <div className="shuriken-loader" />
+      <div className="text-loader" />
     </div>
   );
 }
